8af37dd78135e8d4e8394bda32b183e8dcab5a33
Abhinav Jain <58175317+abhi-146@users.noreply.github.com>
2024-03-14 19:43:36 +0530

    Update index.js

--- Redux/reducers/index.js
+++ Redux/reducers/index.js
@@ -1,1 +1,10 @@
+// reducers/index.js
+import { combineReducers } from 'redux';
+import counterReducer from './counterReducer';
 
+// Combine all reducers into a single rootReducer
+const rootReducer = combineReducers({
+  counter: counterReducer,
+});
+
+export default rootReducer;
